47b4f41cce8b4e964216ae11f6e183fb1e926706
Joshua Debazar <debajoshua@gmail.com>
2024-04-10 14:21:17 +0000

    Store Logic implemented

---
+++ src/store/store.ts
@@ -0,0 +1,15 @@
+export class Store{
+
+  private subscribers: Function[];
+  private reducers: {[key: string]: Function };
+  private state: {[key:string]: any};
+
+  constructor(reducers = {}, initialState = {}){
+    this.state = initialState;
+    this.reducers = reducers;
+  }
+
+  get value(){
+    return this.state;
+  }
+}
