6373268c5c4867470474c1990c07389c10e52cc1
Joshua Debazar <debajoshua@gmail.com>
2024-04-10 18:06:57 +0000

    Reducers logic implemented

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,28 @@
-export class Store{
-
+export class Store {
   private subscribers: Function[];
-  private reducers: {[key: string]: Function };
-  private state: {[key:string]: any};
+  private reducers: { [key: string]: Function };
+  private state: { [key: string]: any };
 
-  constructor(reducers = {}, initialState = {}){
-    this.state = initialState;
+  constructor(reducers = {}, initialState = {}) {
     this.reducers = reducers;
+    this.state = initialState;
   }
 
-  get value(){
+  get value() {
     return this.state;
+  }
+
+  dispatch(action) {
+    this.state = this.reduce(this.state,action);
+  }
+
+  reduce(state,action){
+
+    const newState = {};
+    for(const prop in this.reducers)
+    {
+       newState[prop] = this.reducers[prop](state[prop],action);
+    }
+    return newState;
   }
 }
